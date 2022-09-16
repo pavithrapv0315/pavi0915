2a93f98ecf89c3e65a1b10c4d3c165b03f1ce415
pavithrapv0315 <pavithradhayalan0315@gmail.com>
2022-09-16 19:27:33 +0530

    pavi

---
+++ src/org/test/Sample.java
@@ -0,0 +1,16 @@
+package org.test;
+public class Sample {
+ public void tamil() {
+	 System.out.println("tamil");
+	 }
+ public void english() {
+	System.out.println("english");
+}
+ 
+ public static void main(String[] args) {
+	
+	Sample s = new Sample();
+	s.tamil();
+			s.english();
+}
+}
